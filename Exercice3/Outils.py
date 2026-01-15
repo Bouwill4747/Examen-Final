d12b06fba2158cc048f4e439d1a7cb3f2dfd1f45
Will <Williambourbonniere110@gmail.com>
2026-01-15 15:27:59 -0500

    Création de la fonction input_nombres, on crée une liste de nombre avec 10 nombres entier et on s'assure que le nombre est bien un nombre entier valide, si il y a une erreur, le code le mentionne. Aussi, ajout de la fonction afficher_liste pour afficher la liste de nombres Dans main.py, ajout de outils.afficher_liste() et des prints pour la propreter.

--- Exercice3/Outils.py
+++ Exercice3/Outils.py
@@ -1,9 +1,23 @@
 class Outils:
     def __init__(self):
-        pass
+        self.nombres = []
 
     def input_nombres(self):
-        pass
+        self.nombres = []
+        print("Veuillez entrer 10 nombres entiers")
+
+        for i in range(10):
+            while True:
+                try:
+                    nombre = int(input(f"Nombres {i + 1} : "))
+                    self.nombres.append(nombre)
+                    break
+                except ValueError:
+                    print("Veuillez entrer un nombre entier valide")
+
+    def afficher_liste(self):
+        print("Liste des nombres : ")
+        print(self.nombres)
 
     def min(self):
         pass
