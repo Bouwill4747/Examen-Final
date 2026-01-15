f72af4be447db4ec2dbbc9f99b36ac37dab33f01
Will <Williambourbonniere110@gmail.com>
2026-01-15 16:27:16 -0500

    Création du menu et les choix. Premierement, on demande a l'utilisateur de mettre le nom du personnage, ensuite on afficher les nom et la vie du personnage avec la fonctions personnage.afficher(). Ensuite on a le choix entre 3 input; 1, 2 ou 3. Le premier sert a attaquer, le deuxieme a soigner et le troisieme a quitter le programme. Implementation de systeme d'erreur d'input, le nombre doit etre un nombre entier positif entre 0-100, sinon erreur.

--- Exercice4/main.py
+++ Exercice4/main.py
@@ -8,17 +8,19 @@ class Personnage:
 
         if self.vie <= 0:
             self.vie = 0
-            print(f"{self.nom} a perdu {degats} points de vie.")
+            print(f"{self.nom} a perdu {degats} points de vie.\n")
+            print(50 * "*")
             print(f"{self.nom} est mort. Merci d'avoir jouer :D ")
+            print(50 * "*")
             exit()
         else:
-            print(f"{self.nom} a perdu {degats} points de vie.")
+            print(f"{self.nom} a perdu {degats} points de vie.\n")
 
     def soigner(self, points):
         self.vie += points
         if self.vie > 100:
             self.vie = 100
-        print(f"{self.nom} a gagner {points} points de vie.")
+        print(f"{self.nom} a gagner {points} points de vie.\n")
 
     def afficher(self):
         print(f"\nNom : {self.nom} | Vie : {self.vie}\n")
@@ -28,15 +30,40 @@ personnage = Personnage(nom)
 
 while True:
 
+    print(50 * "-")
+    personnage.afficher()
+    print(50 * "-")
+    print("| Faite un choix parmis les suivants : ")
+    print("| 1. Attaquer le personnage")
+    print("| 2. Soigner le personnage de vie")
+    print("| 3. Quitter")
+    print(50 * "-", "\n")
     choix = input("Votre choix : ")
 
     if choix == "1":
-        break
+        try:
+            degats = int(input("\nCombiens de degats voulez vous faire a votre personnage (0-100) : "))
+            if 0 <= degats <= 100:
+                personnage.attaquer(degats)
+            else:
+                print("\nVeuillez entrez un nombre entre 0 et 100")
+        except ValueError:
+            print("\nVeuillez entrez un nombre entier valide")
 
     elif choix == "2":
-        break
+        try:
+            soin = int(input("\nVous voulez soigner votre personnage de combiens de points (0-100) : "))
+            if 0 <= soin <= 100:
+                personnage.soigner(soin)
+            else:
+                print("\nVeuillez entrez un nombre entre 0 et 100")
+        except ValueError:
+            print("\nVeuillez entrez un nombre entier valide")
 
     elif choix == "3":
+        print(20 * "*")
+        print("Merci d'avoir jouer, Au revoir!")
+        print(20 * "*")
         break
 
     else:
