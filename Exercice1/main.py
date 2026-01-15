607e7824abbc03569086ceb2025c8555ef7a5f1f
Will <Williambourbonniere110@gmail.com>
2026-01-15 13:27:31 -0500

    Création des fonctions dans la class Triangle

--- Exercice1/main.py
+++ Exercice1/main.py
@@ -1,16 +1,19 @@
 class Triangle:
-
     def __init__(self, n):
         self.n = n
 
     def ligne_croissant(self):
-        pass
+        lignes = []
+        for i in range(1, self.n + 1):
+            lignes.append("*" * i)
 
     def ligne_decroissant(self):
-        pass
+        lignes = []
+        for i in range(self.n, 0, -1):
+            lignes.append("*" * i)
+        return lignes
 
 class Affichage:
-
     def afficher(self, triangle):
         pass
 
