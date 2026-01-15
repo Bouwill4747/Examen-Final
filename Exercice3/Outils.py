9b1c5fd8b1bb57a5eee0025ef80dffbd570d7acf
Will <Williambourbonniere110@gmail.com>
2026-01-15 15:14:13 -0500

    Création du fichier Outils.py, et du main.py. Dans le main, création du programme principal, appelle a la class Outils, et créeation des prints. Dans Outils.py, création des foncitons, input_nombres pour que l'utilisateur puisse rentrer ces nombres, min pour Minimum, max pour Maximum, somme et moyenne.

---
+++ Exercice3/Outils.py
@@ -0,0 +1,18 @@
+class Outils:
+    def __init__(self):
+        pass
+
+    def input_nombres(self):
+        pass
+
+    def min(self):
+        pass
+
+    def max(self):
+        pass
+
+    def somme(self):
+        pass
+
+    def moyenne(self):
+        pass
---
+++ Exercice3/main.py
@@ -0,0 +1,12 @@
+from Outils import Outils
+
+outils = Outils()
+outils.input_nombres()
+
+print(50 * "*")
+print("\nResultats :")
+print("Minimum :", outils.min())
+print("Maximum :", outils.max())
+print("Maximum :", outils.max())
+print("Moyenne :", outils.moyenne(), "\n")
+print(50 * "*")
